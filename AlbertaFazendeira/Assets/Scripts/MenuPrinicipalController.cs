f32e7715ce64115cba4640ad2ca4fa4829f81bd2
larajudithe <20241ctb0180010@estudantes.ifpr.edu.br>
2026-04-07 18:32:04 -0300

    VersaoYipieee
    Funciona certinho no celular e ta pronto do 1 ao 6, faltam só 3, yay

--- AlbertaFazendeira/Assets/Scripts/MenuPrinicipalController.cs
+++ AlbertaFazendeira/Assets/Scripts/MenuPrinicipalController.cs
@@ -1,31 +1,62 @@
+using System;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MenuPrinicipalController : MonoBehaviour
 {
-    [SerializeField] public string NomeLevelJogo;
-    [SerializeField] public string NomeMenu;
+    [SerializeField] private string NomeLevelJogo;
+    [SerializeField] private string NomeMenu;
+    [SerializeField] private GameObject painelMenuInicial;
+    [SerializeField] private GameObject painelOpc;
+    //[SerializeField] private GameObject painelMenuInicial;
+    [SerializeField] private GameObject painelSairMsm;
+
     public void Jogar()
     {
         SceneManager.LoadScene(NomeLevelJogo);
     }
 
-     public void Sair()
+    public void Sair()
     {
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+        #else
         Application.Quit();
+        #endif
     }
 
     public void VoltarMenu()
     {
+
         SceneManager.LoadScene(NomeMenu);
     }
 
 
-    //  public void Opcao()
-    // {
-        
-    // }
+    public void OpenOpcao()
+    {
+        painelMenuInicial.SetActive(false);
+        painelOpc.SetActive(true);
+
+    }
+
+    public void CloseOpcao()
+    {
+        painelMenuInicial.SetActive(true);
+        painelOpc.SetActive(false);
+    }
+
+    public void OpenSairOpcao()
+    {
+        painelMenuInicial.SetActive(false);
+        painelSairMsm.SetActive(true);
+    }
+
+    public void CloseSairOpcao()
+    {
+        painelMenuInicial.SetActive(true);
+        painelSairMsm.SetActive(false);
+    }
 }
 
 
